2c0436425ae68418112015ab7e81e76697b4f06b
Stu Field <stu.g.field@gmail.com>
2025-04-04 14:56:25 -0600

    Added unit test for cross-validation
    - new unit tests specifically testing the
      cross-validation setup
    - add checks for setdiff and intersect
      of the train/test indices
    - all test indices are tested exactly once
    - no overlap between train/test within a fold
      instance

--- tests/testthat/test-setup-cross.R
+++ tests/testthat/test-setup-cross.R
@@ -16,6 +16,29 @@ test_that("`setup_cross()` returns correct 2x5 indices for cross-val setup", {
   expect_snapshot(flatten(cv))
 })
 
+test_that("`setup_cross()` returns correct indices for proper cross validation", {
+  # do a single run
+  x$runs <- 1L
+  cv <- setup_cross.feature_select(x)$cross_val |>
+    unlist(recursive = FALSE, use.names = FALSE)
+
+  # ensure that test/train are set unique
+  train_test_intersect <- vapply(cv, function(.x) {
+    identical(
+      intersect(.x$test_rows, .x$training_rows),
+      integer(0)     # expect no overlap
+    )
+  }, NA)
+  expect_true(all(train_test_intersect))
+
+  # ensure that all samples are tested once and only once
+  test_idx <- lapply(cv, `[[`, i = "test_rows") |> unlist()
+  data_idx <- seq_len(nrow(x$data))
+  expect_equal(anyDuplicated(test_idx), 0L) # no duplicates
+  expect_length(test_idx, nrow(x$data))     # one entry for each row
+  expect_setequal(test_idx, data_idx)       # sets are equal
+})
+
 test_that("`setup_cross_strat()` returns stratified setup based on 'vs' column", {
   x$cross_val$strat_column <- "vs"
   x$cross_val$runs  <- x$runs
